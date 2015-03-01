7f2cdc2ecbd12d49f3f60d472028baa54df7105d
SnorlaxNL <rbbentum@gmail.com>
2015-03-01 14:32:10 +0100

    Update 1.1
    GOED

---
+++ js/Warenhuis - RJR.js
@@ -0,0 +1,54 @@
+body
+
+var ddmenuOptions=
+{
+    menuId: "ddmenu",
+    linkIdToMenuHtml: null,
+    open: "onmouseover",  //or "onclick"
+    delay: 90,
+    speed: 0.4,
+    license: "mylicense"
+}
+
+var timeout	= 500;
+var closetimer	= 0;
+var ddmenuitem	= 0;
+
+// open hidden layer
+function mopen(id)
+{	
+	// cancel close timer
+	mcancelclosetime();
+
+	// close old layer
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+
+	// get new layer and show it
+	ddmenuitem = document.getElementById(id);
+	ddmenuitem.style.visibility = 'visible';
+
+}
+// close showed layer
+function mclose()
+{
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+}
+
+// go close timer
+function mclosetime()
+{
+	closetimer = window.setTimeout(mclose, timeout);
+}
+
+// cancel close timer
+function mcancelclosetime()
+{
+	if(closetimer)
+	{
+		window.clearTimeout(closetimer);
+		closetimer = null;
+	}
+}
+
+// close layer when click-out
+document.onclick = mclose; 
